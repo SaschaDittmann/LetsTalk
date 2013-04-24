9c4eff055505902fd01560fdb8a1abd9742e6a12
Sascha Dittmann <Sascha5812@gmx.de>
2013-04-24 16:58:29 +0200

    Initial Commit

---
+++ 01_LetsTalk_WinRT_CRUD/LetsTalk/MainPage.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using LetsTalk.Common;
+using LetsTalk.Model;
+using LetsTalk.ViewModels;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace LetsTalk
+{
+    public sealed partial class MainPage : LayoutAwarePage
+    {
+        private readonly IMainPageViewModel _viewModel ;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _viewModel = new MainPageViewModel();
+            DataContext = _viewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            _viewModel.RefreshCommand.Execute();
+        }
+
+        #region Page state management
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            if (pageState == null)
+            {
+                // When this is a new page, select the first item automatically unless logical page
+                // navigation is being used (see the logical page navigation #region below.)
+                if (!UsingLogicalPageNavigation() && MessagesViewSource.View != null)
+                {
+                    MessagesViewSource.View.MoveCurrentToFirst();
+                }
+            }
+            else
+            {
+                // Restore the previously saved state associated with this page
+                if (pageState.ContainsKey("SelectedItem") && MessagesViewSource.View != null)
+                {
+                    var id = pageState["SelectedItem"] as int?;
+                    if (id.HasValue)
+                    {
+                        var item = MessageListView.Items.FirstOrDefault(i =>
+                            {
+                                var m = i as Message;
+                                return m != null && m.Id == id;
+                            });
+                        if (item != null)
+                            MessagesViewSource.View.MoveCurrentTo(item);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+            if (MessagesViewSource.View != null)
+            {
+                var selectedItem = MessagesViewSource.View.CurrentItem as Message;
+                if (selectedItem != null)
+                    pageState["SelectedItem"] = selectedItem.Id;
+            }
+        }
+
+        #endregion
+
+        #region Logical page navigation
+
+        // Visual state management typically reflects the four application view states directly
+        // (full screen landscape and portrait plus snapped and filled views.)  The split page is
+        // designed so that the snapped and portrait view states each have two distinct sub-states:
+        // either the item list or the details are displayed, but not both at the same time.
+        //
+        // This is all implemented with a single physical page that can represent two logical
+        // pages.  The code below achieves this goal without making the user aware of the
+        // distinction.
+
+        /// <summary>
+        /// Invoked to determine whether the page should act as one logical page or two.
+        /// </summary>
+        /// <param name="viewState">The view state for which the question is being posed, or null
+        /// for the current view state.  This parameter is optional with null as the default
+        /// value.</param>
+        /// <returns>True when the view state in question is portrait or snapped, false
+        /// otherwise.</returns>
+        private bool UsingLogicalPageNavigation(ApplicationViewState? viewState = null)
+        {
+            if (viewState == null) viewState = ApplicationView.Value;
+            return viewState == ApplicationViewState.FullScreenPortrait ||
+                viewState == ApplicationViewState.Snapped;
+        }
+
+        /// <summary>
+        /// Invoked when the page's back button is pressed.
+        /// </summary>
+        /// <param name="sender">The back button instance.</param>
+        /// <param name="e">Event data that describes how the back button was clicked.</param>
+        protected override void GoBack(object sender, RoutedEventArgs e)
+        {
+            if (UsingLogicalPageNavigation() && MessageListView.SelectedItem != null)
+            {
+                // When logical page navigation is in effect and there's a selected item that
+                // item's details are currently displayed.  Clearing the selection will return to
+                // the item list.  From the user's point of view this is a logical backward
+                // navigation.
+                MessageListView.SelectedItem = null;
+            }
+            else
+            {
+                // When logical page navigation is not in effect, or when there is no selected
+                // item, use the default back button behavior.
+                base.GoBack(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Invoked to determine the name of the visual state that corresponds to an application
+        /// view state.
+        /// </summary>
+        /// <param name="viewState">The view state for which the question is being posed.</param>
+        /// <returns>The name of the desired visual state.  This is the same as the name of the
+        /// view state except when there is a selected item in portrait and snapped views where
+        /// this additional logical page is represented by adding a suffix of _Detail.</returns>
+        protected override string DetermineVisualState(ApplicationViewState viewState)
+        {
+            // Determine visual states for landscape layouts based not on the view state, but
+            // on the width of the window.  This page has one layout that is appropriate for
+            // 1366 virtual pixels or wider, and another for narrower displays or when a snapped
+            // application reduces the horizontal space available to less than 1366.
+            if (viewState == ApplicationViewState.Filled ||
+                viewState == ApplicationViewState.FullScreenLandscape)
+            {
+                var windowWidth = Window.Current.Bounds.Width;
+                if (windowWidth >= 1366) return "FullScreenLandscapeOrWide";
+                return "FilledOrNarrow";
+            }
+
+            return base.DetermineVisualState(viewState);
+        }
+
+        #endregion
+        
+    }
+}

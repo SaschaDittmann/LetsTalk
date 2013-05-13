cdbea56eba1cdb25a31de7a4d9db073809059f6e
Peter Nowak <Peter.Nowak@hotmail.de>
2013-05-13 22:08:03 +0200

    Initial WP8 Checkin

---
+++ 01_LetsTalk_WP8_CRUD/LetsTalkWP8/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using LetsTalkWP8.Resources;
+using LetsTalkWP8.ViewModel;
+
+namespace LetsTalkWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private readonly IMainPageViewModel _viewModel;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            _viewModel = new MainPageViewModel();
+            DataContext = _viewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+               base.OnNavigatedTo(e);
+               _viewModel.RefreshCommand.Execute();
+        }
+    }
+}

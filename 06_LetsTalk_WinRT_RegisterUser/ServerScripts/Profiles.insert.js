c767e02f1fe0a73f53208d11150754c8052ad536
Sascha Dittmann <Sascha5812@gmx.de>
2013-06-28 13:07:40 +0200

    Updated to Mobile Services SDK 1.0

--- 06_LetsTalk_WinRT_RegisterUser/ServerScripts/Profiles.insert.js
+++ 06_LetsTalk_WinRT_RegisterUser/ServerScripts/Profiles.insert.js
@@ -1,18 +1,18 @@
-var profiles = tables.getTable('profiles');
-
-function insert(item, user, context) {
-    if (!item.name && item.name.length === 0) {
-        context.respond(statusCodes.BAD_REQUEST, 'A name must be provided');
-        return;
-    }
-
-    if (!item.email && item.email.length === 0) {
-        context.respond(statusCodes.BAD_REQUEST, 'An email address must be provided');
-        return;
-    }
+function insert(item, user, request) {
+    var profiles = tables.getTable('profiles');
 
     if (item.userId !== user.userId) {
-        context.respond(statusCodes.BAD_REQUEST, 'A user can only insert a profile for their own userId.');
+        request.respond(statusCodes.BAD_REQUEST, 'A user can only insert a profile for their own userId.');
+        return;
+    }
+
+    if (!item.name || item.name.length === 0) {
+        request.respond(statusCodes.BAD_REQUEST, 'A name must be provided');
+        return;
+    }
+
+    if (!item.email || item.email.length === 0) {
+        request.respond(statusCodes.BAD_REQUEST, 'An email address must be provided');
         return;
     }
 
@@ -20,13 +20,13 @@ function insert(item, user, context) {
     profiles.where({ userId: item.userId }).read({
         success: function (results) {
             if (results.length > 0) {
-                context.respond(statusCodes.BAD_REQUEST, 'Profile already exists.');
+                request.respond(statusCodes.BAD_REQUEST, 'Profile already exists.');
                 return;
             }
 
-            // No such user exists, add a timestamp and proces the insert
+            // No such user exists, add a timestamp and process the insert
             item.memberSince = new Date();
-            context.execute();
+            request.execute();
         }
     });
 }
